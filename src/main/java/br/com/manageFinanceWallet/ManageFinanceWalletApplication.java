2f6faa1be4c4dc1e5b808f2217799fb174a8ba88
francomatheus <matheus_2222@hotmail.com>
2020-04-30 22:14:10 -0300

    Criando o projeto spring para a api

---
+++ src/main/java/br/com/manageFinanceWallet/ManageFinanceWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.manageFinanceWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageFinanceWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageFinanceWalletApplication.class, args);
+	}
+
+}
