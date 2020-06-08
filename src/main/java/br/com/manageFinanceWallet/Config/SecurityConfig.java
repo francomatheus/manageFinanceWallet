d13ba400d96f4671ea9cecaa3253288443521efd
francomatheus <matheus_2222@hotmail.com>
2020-06-08 11:46:35 -0300

    Configurando swagger 2

--- src/main/java/br/com/manageFinanceWallet/Config/SecurityConfig.java
+++ src/main/java/br/com/manageFinanceWallet/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import br.com.manageFinanceWallet.Security.JwtTokenFilter;
 import br.com.manageFinanceWallet.token.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationService authenticationService;
@@ -34,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .antMatchers("/swagger-ui.html","/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
